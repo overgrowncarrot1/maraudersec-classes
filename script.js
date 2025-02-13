d0dae849d12ce5454971960dbd89268246a6a2f0
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2025-02-12 22:02:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     checkLogin();
+    loadProgress();
 });
+
+let currentModule = 0;
+const modules = {
+    "Enumeration": ["nmap", "nse", "rustscan", "finding_users"],
+};
 
 function toggleMenu(element) {
     let submenu = element.nextElementSibling;
@@ -26,11 +32,23 @@ function loadModule(module) {
 }
 
 function prevModule() {
-    alert("Previous module functionality coming soon!");
+    if (currentModule > 0) {
+        currentModule--;
+        loadModule(modules["Enumeration"][currentModule]);
+    } else {
+        alert("You are already at the first module in this section.");
+    }
 }
 
 function nextModule() {
-    alert("Next module functionality coming soon!");
+    if (currentModule < modules["Enumeration"].length - 1) {
+        currentModule++;
+        loadModule(modules["Enumeration"][currentModule]);
+    } else {
+        // If it's the last module in the current section, move to the next section (Tab)
+        alert("You're at the last module in this section. Moving to the next section.");
+        // You can implement a mechanism to load the next section here (like switching tabs).
+    }
 }
 
 function login() {
