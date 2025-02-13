726c0e14dd7119a69bd765277df2c11bf09df295
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2025-02-12 21:51:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     checkLogin();
+    loadProgress();
 });
+
+const modules = ["Enumeration", "Nmap", "NSE", "RustScan", "Finding Users"]; // Define the order of modules
 
 function toggleMenu(element) {
     let submenu = element.nextElementSibling;
@@ -13,11 +16,29 @@ function loadModule(module) {
 }
 
 function prevModule() {
-    alert("Previous module functionality coming soon!");
+    let currentModule = localStorage.getItem("currentModule");
+    let currentIndex = modules.indexOf(currentModule);
+    
+    if (currentIndex > 0) {
+        let prevModule = modules[currentIndex - 1];
+        loadModule(prevModule);
+        localStorage.setItem("currentModule", prevModule); // Save the current module
+    } else {
+        alert("You are already at the first module!");
+    }
 }
 
 function nextModule() {
-    alert("Next module functionality coming soon!");
+    let currentModule = localStorage.getItem("currentModule");
+    let currentIndex = modules.indexOf(currentModule);
+    
+    if (currentIndex < modules.length - 1) {
+        let nextModule = modules[currentIndex + 1];
+        loadModule(nextModule);
+        localStorage.setItem("currentModule", nextModule); // Save the current module
+    } else {
+        alert("You have reached the last module!");
+    }
 }
 
 function login() {
@@ -44,6 +65,7 @@ function saveProgress(module) {
     let username = localStorage.getItem("username");
     if (username) {
         localStorage.setItem(`progress-${username}`, module);
+        localStorage.setItem("currentModule", module); // Save the current module
     }
 }
 
@@ -53,6 +75,7 @@ function loadProgress() {
         let lastModule = localStorage.getItem(`progress-${username}`);
         if (lastModule) {
             loadModule(lastModule);
+            localStorage.setItem("currentModule", lastModule); // Ensure currentModule is loaded
         }
     }
 }
